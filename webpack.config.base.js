a8b7a88e48b131ec5975dd661fc6e19d6c41df17
HuangTbb <1141397857@qq.com>
2021-10-07 12:05:36 +0800

    引入scss\less\stylus\图片\懒加载

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -11,5 +11,38 @@ module.exports = {
             title: '写代码啦',
             template: 'src/assets/index.html'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.styl$/,
+                loader: ['style-loader','css-loader','stylus-loader']
+            },
+            {
+                test: /\.less$/i,
+                loader: ['style-loader','css-loader','less-loader']
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require('dart-sass')
+                        }
+                    }
+                ]
+            }
+        ]
+    }
 }
